6352db590561300e0c517443f173cbfec98491f4
xinzz <alfinoarumi@gmail.com>
2026-01-14 14:48:45 +0700

    Menambahkan Tambah, Kurang, Kali, Bagi

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int a = 10, b = 2, c = 0;
+
+void Tambah()
+{
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+
+void Kurang()
+{
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+void Kali()
+{
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+void Bagi()
+{
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+Tambah();
+Kurang();
+Tambah();
+Kali();
+Bagi();
